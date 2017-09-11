f9eb5c0941ad582a283feb65e569ddf76f08e2a8
151250064 <151250064@smail.nju.edu.cn>
2017-09-11 16:13:23 +0800

    主框架写好了

--- src/routes/MainPanelPage.js
+++ src/routes/MainPanelPage.js
@@ -3,68 +3,56 @@
  */
 
 import React from 'react';
-import {Layout, Menu, Breadcrumb, Icon} from 'antd';
 import styles from './MainPanelPage.css';
-const {SubMenu} = Menu;
-const {Header, Content, Sider} = Layout;
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
 
-class MainPanelPage extends React.component {
+class MainPanelPage extends React.Component {
 
   render() {
     return(
       <Layout>
-        <Header className="header">
-          <div className={styles.logo} />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={['2']}
-            style={{ lineHeight: '64px' }}
-          >
-            <Menu.Item key="1">nav 1</Menu.Item>
-            <Menu.Item key="2">nav 2</Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
-          </Menu>
+        <Header className="header" style={{ background: '#fff', height: 80 }}>
+          <div className={styles.logo}/>
+          <div className={styles.logo2}/>
         </Header>
-        <Layout>
-          <Sider width={200} style={{ background: '#fff' }}>
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              style={{ height: '100%', borderRight: 0 }}
-            >
-              <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-                <Menu.Item key="1">option1</Menu.Item>
-                <Menu.Item key="2">option2</Menu.Item>
-                <Menu.Item key="3">option3</Menu.Item>
-                <Menu.Item key="4">option4</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-                <Menu.Item key="5">option5</Menu.Item>
-                <Menu.Item key="6">option6</Menu.Item>
-                <Menu.Item key="7">option7</Menu.Item>
-                <Menu.Item key="8">option8</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-                <Menu.Item key="9">option9</Menu.Item>
-                <Menu.Item key="10">option10</Menu.Item>
-                <Menu.Item key="11">option11</Menu.Item>
-                <Menu.Item key="12">option12</Menu.Item>
-              </SubMenu>
-            </Menu>
-          </Sider>
-          <Layout style={{ padding: '0 24px 24px' }}>
-            <Breadcrumb style={{ margin: '12px 0' }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
-            <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+        <Content style={{ padding: '25px 50px 25px 50px', background: '#F0F2F5' }}>
+          <Layout style={{ padding: '24px 0', background: '#fff' }}>
+            <Sider width={200} style={{ background: '#fff' }}>
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%' }}
+              >
+                <SubMenu key="sub1" title={<span><Icon type="user" />信息认证</span>}>
+                  <Menu.Item key="1">基本信息认证</Menu.Item>
+                  <Menu.Item key="2">经济水平录入</Menu.Item>
+                  <Menu.Item key="3">工行账户绑定</Menu.Item>
+                  <Menu.Item key="4">学校教务网认证</Menu.Item>
+                  <Menu.Item key="5">学信网认证</Menu.Item>
+                  <Menu.Item key="6">芝麻信用认证</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" title={<span><Icon type="laptop" />信用评估报告</span>}>
+                </SubMenu>
+                <SubMenu key="sub3" title={<span><Icon type="notification" />投资与借款</span>}>
+                  <Menu.Item key="7">投资</Menu.Item>
+                  <Menu.Item key="8">借款</Menu.Item>
+                  <Menu.Item key="9">还款</Menu.Item>
+                </SubMenu>
+              </Menu>
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: 418 }}>
               Content
             </Content>
           </Layout>
-        </Layout>
+        </Content>
+        <Footer style={{ textAlign: 'center', background: '#404040' }}>
+          <div className={styles.footerWords}>
+            Easy Lend ©2017 Created by NJU team
+          </div>
+        </Footer>
       </Layout>
     );
   }
