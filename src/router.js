2a7ec5a917688eba49314fc9df26b150d2474407
151250064 <151250064@smail.nju.edu.cn>
2017-09-12 23:08:34 +0800

    e-charts

--- src/router.js
+++ src/router.js
@@ -9,6 +9,7 @@ import SchoolAuthPage from './routes/SchoolAuthPage';
 import ICBCCardAuthPage from './routes/ICBCCardAuthPage';
 import XueXinWangAuthPage from './routes/XueXinWangAuthPage';
 import CreditReportPage from './routes/CreaditReportPage';
+import CreditChartsPage from './routes/CreditChartsPage';
 
 function RouterConfig({ history }) {
   return (
@@ -23,6 +24,7 @@ function RouterConfig({ history }) {
         <Route path="/auth/xuexinAuth" component={XueXinWangAuthPage} />
         <Route path="/auth/basicAuth" component={BasicAuthPage} />
         <Route path="/auth/creditReport" component={CreditReportPage}/>
+        <Route path="/auth/creditCharts" component={CreditChartsPage}/>
       </Route>
       <Route  path="/test" component={BasicAuthPage} />
     </Router>
