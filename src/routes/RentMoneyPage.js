159dda10e20554668b9a13f1420aa75d324ecb0c
151250064 <151250064@smail.nju.edu.cn>
2017-09-14 23:44:40 +0800

    借款写好了

--- src/routes/RentMoneyPage.js
+++ src/routes/RentMoneyPage.js
@@ -4,35 +4,78 @@
 
 import React from 'react';
 import PageHeader from '../components/PageHeader.js';
-import { Form, Input, Tooltip, Icon, Cascader, Select, Row, Col, Checkbox, Button, AutoComplete } from 'antd';
+import { Form, Input, Tooltip, Icon, Select, Row, Col, Checkbox, Button } from 'antd';
 const FormItem = Form.Item;
 const Option = Select.Option;
+const { TextArea } = Input;
 
 
-const residences = [{
-  value: 'zhejiang',
-  label: 'Zhejiang',
-  children: [{
-    value: 'hangzhou',
-    label: 'Hangzhou',
-    children: [{
-      value: 'xihu',
-      label: 'West Lake',
-    }],
-  }],
-}, {
-  value: 'jiangsu',
-  label: 'Jiangsu',
-  children: [{
-    value: 'nanjing',
-    label: 'Nanjing',
-    children: [{
-      value: 'zhonghuamen',
-      label: 'Zhong Hua Men',
-    }],
-  }],
-}];
+class PriceInput extends React.Component {
+  constructor(props) {
+    super(props);
 
+    const value = this.props.value || {};
+    this.state = {
+      number: value.number || 0,
+      currency: value.currency || 'rmb',
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    // Should be a controlled component.
+    if ('value' in nextProps) {
+      const value = nextProps.value;
+      this.setState(value);
+    }
+  }
+  handleNumberChange = (e) => {
+    const number = parseInt(e.target.value || 0, 10);
+    if (isNaN(number)) {
+      return;
+    }
+    if (!('value' in this.props)) {
+      this.setState({ number });
+    }
+    this.triggerChange({ number });
+  };
+  handleCurrencyChange = (currency) => {
+    if (!('value' in this.props)) {
+      this.setState({ currency });
+    }
+    this.triggerChange({ currency });
+  };
+  triggerChange = (changedValue) => {
+    // Should provide an event to pass value to Form.
+    const onChange = this.props.onChange;
+    if (onChange) {
+      onChange(Object.assign({}, this.state, changedValue));
+    }
+  };
+
+  render() {
+    const { size } = this.props;
+    const state = this.state;
+    return (
+      <span>
+        <Input
+          type="text"
+          size={size}
+          value={state.number}
+          onChange={this.handleNumberChange}
+          style={{ width: '65%', marginRight: '3%' }}
+        />
+        <Select
+          value={state.currency}
+          size={size}
+          style={{ width: '32%' }}
+          onChange={this.handleCurrencyChange}
+        >
+          <Option value="rmb">RMB</Option>
+          <Option value="dollar">Dollar</Option>
+        </Select>
+      </span>
+    );
+  }
+}
 
 class RentMoneyPage extends React.Component {
   state = {
@@ -46,6 +89,14 @@ class RentMoneyPage extends React.Component {
         console.log('Received values of form: ', values);
       }
     });
+  };
+
+  checkPrice = (rule, value, callback) => {
+    if (value.number > 0) {
+      callback();
+      return;
+    }
+    callback('目标借款金额必须大于0');
   };
 
   render() {
@@ -73,15 +124,6 @@ class RentMoneyPage extends React.Component {
         },
       },
     };
-    const prefixSelector = getFieldDecorator('prefix', {
-      initialValue: '86',
-    })(
-      <Select style={{ width: 60 }}>
-        <Option value="86">+86</Option>
-        <Option value="87">+87</Option>
-      </Select>
-    );
-
 
     return (
       <div>
@@ -89,100 +131,80 @@ class RentMoneyPage extends React.Component {
         <Form onSubmit={this.handleSubmit}>
           <FormItem
             {...formItemLayout}
-            label="E-mail"
-            hasFeedback
-          >
-            {getFieldDecorator('email', {
+            label="目标借款金额">
+            {getFieldDecorator('price', {
+              initialValue: { number: 0, currency: 'rmb' },
               rules: [{
-                type: 'email', message: 'The input is not valid E-mail!',
+                required: true, message: '请输入目标借款金额',
               }, {
-                required: true, message: 'Please input your E-mail!',
+                validator: this.checkPrice,
               }],
-            })(
-              <Input />
-            )}
+            })(<PriceInput />)}
           </FormItem>
+
           <FormItem
             {...formItemLayout}
-            label="Password"
+            label="目标借款银行"
             hasFeedback
           >
-            {getFieldDecorator('password', {
-              rules: [{
-                required: true, message: 'Please input your password!',
-              }, {
-                validator: this.checkConfirm,
-              }],
+            {getFieldDecorator('select', {
+              rules: [
+                { required: true, message: '请选择目标借款银行' },
+              ],
             })(
-              <Input type="password" />
+              <Select placeholder="请选择目标借款银行">
+                <Option value="construction bank">中国建设银行</Option>
+                <Option value="industry bank">中国工商银行</Option>
+                <Option value="agriculture bank">中国农业银行</Option>
+                <Option value="commercial bank">招商银行</Option>
+                <Option value="business bank">兴业银行</Option>
+
+              </Select>
             )}
           </FormItem>
+
           <FormItem
             {...formItemLayout}
-            label={(
-              <span>
-              Nickname&nbsp;
-                <Tooltip title="What do you want other to call you?">
-                <Icon type="question-circle-o" />
-              </Tooltip>
-            </span>
-            )}
+            label="还款期限"
             hasFeedback
           >
-            {getFieldDecorator('nickname', {
-              rules: [{ required: true, message: 'Please input your nickname!', whitespace: true }],
+            {getFieldDecorator('select', {
+              rules: [
+                { required: true, message: '请选择还款期限' },
+              ],
             })(
-              <Input />
+              <Select placeholder="请选择还款期限">
+                <Option value="one">1个月</Option>
+                <Option value="three">3个月</Option>
+                <Option value="six">6个月</Option>
+                <Option value="twelve">12个月</Option>
+              </Select>
             )}
           </FormItem>
+
           <FormItem
             {...formItemLayout}
-            label="Habitual Residence"
+            label="个人借款说明"
+            hasFeedback
           >
-            {getFieldDecorator('residence', {
-              initialValue: ['zhejiang', 'hangzhou', 'xihu'],
-              rules: [{ type: 'array', required: true, message: 'Please select your habitual residence!' }],
+            {getFieldDecorator('select', {
+              rules: [
+                { required: true, message: '请输入个人借款说明' },
+              ],
             })(
-              <Cascader options={residences} />
+              <TextArea rows={4} />
             )}
           </FormItem>
-          <FormItem
-            {...formItemLayout}
-            label="Phone Number"
-          >
-            {getFieldDecorator('phone', {
-              rules: [{ required: true, message: 'Please input your phone number!' }],
-            })(
-              <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
-            )}
-          </FormItem>
-          <FormItem
-            {...formItemLayout}
-            label="Captcha"
-            extra="We must make sure that your are a human."
-          >
-            <Row gutter={8}>
-              <Col span={12}>
-                {getFieldDecorator('captcha', {
-                  rules: [{ required: true, message: 'Please input the captcha you got!' }],
-                })(
-                  <Input size="large" />
-                )}
-              </Col>
-              <Col span={12}>
-                <Button size="large">Get captcha</Button>
-              </Col>
-            </Row>
-          </FormItem>
+
           <FormItem {...tailFormItemLayout} style={{ marginBottom: 8 }}>
             {getFieldDecorator('agreement', {
               valuePropName: 'checked',
             })(
-              <Checkbox>I have read the <a href="">agreement</a></Checkbox>
+              <Checkbox>我已阅读并同意<a href="">借款协议</a></Checkbox>
             )}
           </FormItem>
           <FormItem {...tailFormItemLayout}>
-            <Button type="primary" htmlType="submit">Register</Button>
+            <Button type="primary" htmlType="submit">提交</Button>
           </FormItem>
         </Form>
       </div>
